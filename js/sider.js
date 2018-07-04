db7029dfecd2ee17e6af6f2b306c4173ab8206ff
chenbigcur <876786569@qq.com>
2018-07-04 20:54:15 +0800

    pull

--- js/sider.js
+++ js/sider.js
@@ -1,27 +1,16 @@
-$(document).ready(function () {
-    loadsider();
-});
-
-function loadsider() {
-    let bgnumran = 3;
-    bgnumran = parseInt(Math.random() * bgnumran) + 1;
-    let url = "url(img/bg/blurry/" + bgnumran + ".jpg)";
-    $("body").css("background", url);
-    $("body").css("background-size", "cover");
+(function loadsider() {
+    let link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = "css/sider.css"
+    $("head").append(link);
     $.ajax({
         type: "post",
         url: "sider.html",
         // data: "data",
         // dataType: "dataType",
         success: function (response) {
-            $("body").html(response);
+            $("#sider").html(response);
         }
     }).then((result) => {
-        let link = document.createElement("link");
-        link.rel = "stylesheet";
-        link.href = "css/sider.css"
-        $("head").append(link);
-    }).catch((err) => {
-
-    });
-}
+    })
+})()
