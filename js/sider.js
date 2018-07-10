c6b08b177ee774d54d7658ff622a5b1c8248fad8
chenbigcur <876786569@qq.com>
2018-07-10 20:54:54 +0800

    pull school 1.10

--- js/sider.js
+++ js/sider.js
@@ -1,8 +1,17 @@
-(function loadsider() {
+let iconfont_src = "//at.alicdn.com/t/font_707735_7yv2zfewto2.js"
+
+$(document).ready(function () {
+    loadsider();
+});
+
+function loadsider() {
     let link = document.createElement("link");
     link.rel = "stylesheet";
     link.href = "css/sider.css"
     $("head").append(link);
+    let script = document.createElement("script");
+    script.src = iconfont_src;
+    $("head").append(script);
     $.ajax({
         type: "post",
         url: "sider.html",
@@ -12,5 +21,17 @@
             $("#sider").html(response);
         }
     }).then((result) => {
+        $(".button_ico").click(function () {
+            $("#sider").css({
+                left: '0'
+            });
+            $(".button_ico").slideToggle();
+        });
+        $("#sider").mouseleave(function () {
+            $("#sider").css({
+                left: '-250px'
+            });
+            $(".button_ico").slideToggle();
+        });
     })
-})()
+}
